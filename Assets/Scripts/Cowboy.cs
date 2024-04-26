42c477d6c09e0fde56c7d686bc8e85298b5d2f86
isdi12 <ismaelsesma11@gmail.com>
2024-04-26 12:35:26 +0200

    cowboy
    script del cowboy hecho

--- Assets/Scripts/Cowboy.cs
+++ Assets/Scripts/Cowboy.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
-public class Cowboy : MonoBehaviour
+public class Cowboy : Character
 {
-    // Start is called before the first frame update
-    void Start()
+    public Cowboy(string name, float damage) : base(name, damage, Resources.Load<Sprite>("Sprites/cowboy")) // construimos padre
     {
-        
+       
     }
-
-    // Update is called once per frame
-    void Update()
+    public override float Heal()
     {
-        
+        Debug.Log("cowboy se cura");
+        health += 10;
+        base.Heal();
+        return 10;
+    }
+    public override float Attack()
+    {
+        Debug.Log("Cowboy ataca");
+        return Random.Range(damage , damage * 1.5f);
     }
 }
